400b5210e8a9e0168c7f8192c84c3d728b2d5d5e
Ratul Buragohain <ratul75@hotmail.com>
2022-12-24 12:21:57 +0000

    Initial commit

---
+++ client/script.js
@@ -0,0 +1,89 @@
+import bot from "./assets/robot.webp";
+import user from "./assets/person.jpg";
+
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = "";
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
+  }, 300);
+}
+
+function typeText(element, text) {
+  let index = 0;
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+function generateUniqueId() {
+  return `id-${Date.now()}-${Math.random().toString(16)}`;
+}
+
+function chatStripe(isAlchemist, value, uniqueId) {
+  return `
+      <div class="wrapper ${isAlchemist && "ai"}">
+        <div class="chat">
+          <div class="profile">
+          <img src="${isAlchemist ? bot : user}"  
+          alt="${isAlchemist ? "bot" : "user"}"/>
+          </div>
+          <div class="message" id="${uniqueId}">${value}</div>
+        </div>
+      </div
+
+    `;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+  //User's query
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+  form.reset();
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+  chatContainer.scrollTop += chatContainer.scrollHeight;
+  const messageDiv = document.getElementById(uniqueId);
+  loader(messageDiv);
+  const response = await fetch("http://localhost:5000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = "";
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+    console.log(err);
+    alert(err);
+  }
+};
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    handleSubmit(e);
+  }
+});
