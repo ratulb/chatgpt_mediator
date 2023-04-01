b6350165eede2e8c18754c33cb627f02a77bea70
Ratul <ratul75@hotmail.com>
2023-04-02 01:00:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.post("/", async (req, res) => {
   try {
     const prompt = req.body.prompt;
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: "gpt-3.5-turbo",
       prompt: `${prompt}`,
       temperature: 0.1,
       max_tokens: 2000,
